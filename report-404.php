ff3c3f7126eb5ae2a13d41ce05f290bfc73b9f36
James Cole <thegrumpydictator@gmail.com>
2018-01-24 06:31:14 +0100

    Basic script to submit 404 errors from GitHub

---
+++ report-404.php
@@ -0,0 +1,15 @@
+<?php
+
+$ignore = [
+];
+$page   = $_GET['page']
+
+if (!in_array($page, $ignore)) {
+    $message = "Hi there!\n\nThe following page was not found on firefly-iii.github.io:\n\n%s\n\nIt was requested by: %s.\n\nPlease make a note of it.";
+    $message = sprintf($message, $page, $_SERVER['REMOTE_ADDR']);
+    $headers = 'From: robot@firefly-iii.org' . "\r\n" .
+               'Reply-To: robot@firefly-iii.org' . "\r\n" .
+               'X-Mailer: PHP/' . phpversion();
+    mail('thegrumpydictator@gmail.com', '404 found on firefly-iii.github.io', $message, $headers);
+}
+
