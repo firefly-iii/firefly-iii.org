9a03d90a8a28b3b0e026ed58e5401809bfd74bad
James Cole <thegrumpydictator@gmail.com>
2018-01-31 06:24:32 +0100

    Expand report 404 script.

--- report-404.php
+++ report-404.php
@@ -1,14 +1,23 @@
 <?php
 
-$ignore = [];
-$page   = $_GET['page'] ?? '';
+$content = file_get_contents(__DIR__ . '/storage/hits.txt');
+$ignore  = explode("\n", $content);
+$page    = $_GET['page'] ?? '';
+$ref     = $_GET['ref'] ?? '';
 
 if (!in_array($page, $ignore) && strlen($page) > 0) {
-    $message = "Hi there!\n\nThe following page was not found on firefly-iii.github.io:\n\n%s\n\nIt was requested by: %s.\n\nPlease make a note of it.";
-    $message = sprintf($message, $page, $_SERVER['REMOTE_ADDR']);
+    $message
+        = "Hi there!\n\nThe following page was not found on firefly-iii.github.io:\n\n%s\n\nIt was requested by: %s.\nReferer: %s\n\nPlease make a note of it.";
+    $message = sprintf($message, $page, $_SERVER['REMOTE_ADDR'], $ref);
     $headers = 'From: robot@firefly-iii.org' . "\r\n" .
                'Reply-To: robot@firefly-iii.org' . "\r\n" .
                'X-Mailer: PHP/' . phpversion();
     mail('thegrumpydictator@gmail.com', '404 found on firefly-iii.github.io', $message, $headers);
+
+    // expand ignore file:
+    $ignore[] = $page;
+    $new      = join("\n", $page);
+    file_put_contents(__DIR__ . '/storage/hits.txt', $new);
+
 }
 
