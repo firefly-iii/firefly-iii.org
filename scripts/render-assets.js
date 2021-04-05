25f490c3546e0f726a55ca6f5bfba04866c984bd
James Cole <james@firefly-iii.org>
2021-04-05 12:03:47 +0200

    Add some stuff.

--- scripts/render-assets.js
+++ scripts/render-assets.js
@@ -7,5 +7,14 @@ module.exports = function renderAssets() {
     const sourcePath = upath.resolve(upath.dirname(__filename), '../src/assets');
     const destPath = upath.resolve(upath.dirname(__filename), '../dist/.');
 
-    sh.cp('-R', sourcePath, destPath)
+    sh.cp('-R', sourcePath, destPath);
+
+    // also copy some static files:
+    const staticSourcePath = upath.resolve(upath.dirname(__filename), '../src');
+    const staticDestPath   = upath.resolve(upath.dirname(__filename), '../dist/.');
+
+    sh.cp(staticSourcePath + '/404.html', staticDestPath + '/404.html');
+    sh.cp(staticSourcePath + '/thanks.html', staticDestPath + '/thanks.html');
+    sh.cp(staticSourcePath + '/robots.txt', staticDestPath + '/robots.txt');
+    sh.cp('-R', staticSourcePath + '/.well-known', staticDestPath + '/.well-known');
 };
